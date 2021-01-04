0262d4f6a7a54c97d306d6d440dad5c570d610f9
dengcs <ldbmcs@gmail.com>
2021-01-04 20:30:17 +0800

    update

--- docs/.vuepress/config/sidebar.js
+++ docs/.vuepress/config/sidebar.js
@@ -1,21 +1,21 @@
 module.exports =
     [
-        {
-            title: "Java",
-            collapsable: false,
-            path: '/md/java/',
-            sidebarDepth: 3,
-            children: [
-                {
-                    title: "Java 基础",
-                    collapsable: false,
-                    path: '/md/java/Java 基础/',
-                    sidebarDepth: 2,
-                    children: [
-                        '/md/java/Java 基础/10分钟看懂Java NIO 底层原理',
-                        '/md/java/Java 基础/Java CompletableFuture 详解'
-                    ]
-                }
-            ]
-        }
+        // {
+        //     title: "Java",
+        //     collapsable: false,
+        //     path: '/md/java/',
+        //     sidebarDepth: 3,
+        //     children: [
+        //         {
+        //             title: "Java 基础",
+        //             collapsable: false,
+        //             path: '/md/java/Java 基础/',
+        //             sidebarDepth: 2,
+        //             children: [
+        //                 '/md/java/Java 基础/10分钟看懂Java NIO 底层原理',
+        //                 '/md/java/Java 基础/Java CompletableFuture 详解'
+        //             ]
+        //         }
+        //     ]
+        // }
     ]
